66a66377dcee417a8944f39430df81ea2d128e31
Valdnet <valdnet@itvix.pl>
2021-03-18 17:41:36 +0100

    Add language PL
    Files with Polish language translation.

---
+++ l10n/pl.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+	'files_linkeditor',
+	{
+		'You are about to visit:': 'Masz zamiar odwiedzić:',
+		'Saving failed!': 'Zapisywanie nie powiodło się!',
+		'Edit link': 'Edytuj link',
+		'View link': 'Wyświetl link',
+		'This link-file doesn\'t seem to be valid. – You can fix this by editing the file.': 'Ten plik linku wydaje się być nieprawidłowy. - Możesz to naprawić, edytując plik.',
+		'A slight problem': 'Mały problem',
+		'Cancel': 'Anuluj',
+		'Visit link': 'Odwiedź link',
+		'An error occurred!': 'Wystąpił błąd!',
+		'Link target URL': 'Link docelowy URL',
+		'e.g. https://example.org': 'np. https://przyklad.org',
+		'Save': 'Zapisz',
+		'New link': 'Nowy link',
+		'Link.URL': 'Link.URL',
+		'Link.webloc': 'Link.webloc',
+		'Open in same window': 'Otwórz w tym samym oknie',
+		'Skip confirmation dialog before open (has to open in same window)': 'Pomiń okno dialogowe potwierdzenia przed otwarciem (musi być otwarte w tym samym oknie)'
+	},
+'nplurals=2; plural=(n != 1);');
