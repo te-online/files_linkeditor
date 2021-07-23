2f925ab1da74da0f4594f5814921bcae0f23d3de
p-bo <pavel.borecki@gmail.com>
2021-07-23 13:50:36 +0200

    Add Czech localization - .js variant

---
+++ l10n/cs.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+	'files_linkeditor',
+	{
+		'You are about to visit:': 'Chystáte se otevřít tento odkaz:',
+		'Saving failed!': 'Uložení se nezdařilo!',
+		'Edit link': 'Upravit odkaz',
+		'View link': 'Zobrazit odkaz',
+		'This link-file doesn\'t seem to be valid. – You can fix this by editing the file.': 'Tento soubor s odkazem se nezdá být platný. – Je možné to napravit jeho upravením.',
+		'A slight problem': 'Malý problém',
+		'Cancel': 'Storno',
+		'Visit link': 'Navštívit odkaz',
+		'An error occurred!': 'Došlo k chybě!',
+		'Link target URL': 'URL adresa cíle odkazu',
+		'e.g. https://example.org': 'např. https://example.org',
+		'Save': 'Uložit',
+		'New link': 'Nový odkaz',
+		'Link.URL': 'Link.URL',
+		'Open in same window': 'Otevřít ve stejném okně',
+		'Skip confirmation dialog before open (has to open in same window)': 'Přeskočit potvrzovací dialog před otevřením (je třeba, aby se otevřelo ve stejném okně)'
+	},
+'nplurals=2; plural=(n != 1);');
